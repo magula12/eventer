6cb3605f0939e6f410ea1a4888d9914a067a7448
magul <magula12@uniba.sk>
2024-11-02 10:08:29 +0100

    eventList.py added

---
+++ python/models/tests/test_eventList.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from datetime import date, time
+
+from ..event import Event
+from ..eventCategory import EventCategory
+from ..eventList import EventList
+
+
+class TestEventList(TestCase):
+
+    def setUp(self):
+        self.event1 = Event(1, EventCategory(123,"Meeting","fakt huste",3), 1, "Event 1", date(2023, 10, 1), time(10, 0), "Description 1")
+        self.event2 = Event(2, EventCategory(13,"Workshop", "nuda",4), 2, "Event 2", date(2023, 10, 2), time(11, 0), "Description 2")
+        self.event_list = EventList([self.event1, self.event2])
+
+    def test_add_event(self):
+        event3 = Event(3, EventCategory(12,"Conference",'daka konfera', 7), 3, "Event 3", date(2023, 10, 3), time(12, 0), "Description 3")
+        self.event_list.add_event(event3)
+        self.assertIn(event3, self.event_list.get_events())
+
+    def test_remove_event_by_index(self):
+        self.event_list.remove_event(0)
+        self.assertNotIn(self.event1, self.event_list.get_events())
+
+    def test_remove_event_by_object(self):
+        self.event_list.remove_event(self.event1)
+        self.assertNotIn(self.event1, self.event_list.get_events())
+
+    def test_remove_event_invalid_index(self):
+        with self.assertRaises(IndexError):
+            self.event_list.remove_event(10)
+
+    def test_remove_event_not_in_list(self):
+        event3 = Event(3, EventCategory(12,"Conference",'daka konfera', 7), 3, "Event 3", date(2023, 10, 3), time(12, 0), "Description 3")
+        with self.assertRaises(ValueError):
+            self.event_list.remove_event(event3)
+
+    def test_get_event(self):
+        self.assertEqual(self.event_list.get_event(0), self.event1)
+        self.assertEqual(self.event_list.get_event(1), self.event2)
+
+    def test_get_events(self):
+        self.assertEqual(self.event_list.get_events(), [self.event1, self.event2])
